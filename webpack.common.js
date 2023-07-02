68f1fe6a6b97c9bac9b87c244e35f2a33a427fca
yourLogic01 <2010631170004@student.unsika.ac.id>
2023-07-02 21:08:57 +0700

    bundle

--- webpack.common.js
+++ webpack.common.js
@@ -4,7 +4,6 @@ const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 const path = require('path');
 const ImageminWebpackPlugin = require('imagemin-webpack-plugin').default;
 const ImageminMozjpeg = require('imagemin-mozjpeg');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   entry: {
@@ -78,6 +77,5 @@ module.exports = {
         }),
       ],
     }),
-    new BundleAnalyzerPlugin(),
   ],
 };
